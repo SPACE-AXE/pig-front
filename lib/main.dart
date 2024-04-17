a0ca5c074ddcf48b14488273f2a158df2c5dc0fe
ipmango <103362844+iipmango@users.noreply.github.com>
2024-04-17 22:03:22 +0900

    Revert "Feature/auth"

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:appfront/Screen/Auth/login/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,36 +9,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
+    return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height * 0.4),
-              const GoToLogin()
-            ],
-          ),
+          child: Text('Hello World!'),
         ),
       ),
     );
-  }
-}
-
-class GoToLogin extends StatelessWidget {
-  const GoToLogin({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-        onPressed: () => {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const LoginScreen()))
-            },
-        child: const Text("go to login"));
   }
 }
