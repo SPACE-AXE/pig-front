0a5e3772e151bb3f017cad84f88aa2ec87e63cb7
권수열 <ipmango@gwonsuyeol-ui-MacBookAir.local>
2024-04-21 16:21:47 +0900

    add naver map dependencies

---
+++ lib/Screen/Auth/map/map_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: const Center(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:appfront/Screen/Auth/login/login_screen.dart';
+import 'package:appfront/Screen/Auth/map/map_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,11 +20,27 @@ class MainApp extends StatelessWidget {
               SizedBox(
                   width: MediaQuery.of(context).size.width,
                   height: MediaQuery.of(context).size.height * 0.4),
-              const GoToLogin()
+              const GoToLogin(),
+              const GoToMap()
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class GoToMap extends StatelessWidget {
+  const GoToMap({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const MapScreen()));
+      },
+      child: const Text("go to map"),
     );
   }
 }
@@ -36,10 +53,11 @@ class GoToLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-        onPressed: () => {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const LoginScreen()))
-            },
-        child: const Text("go to login"));
+      onPressed: () => {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const LoginScreen()))
+      },
+      child: const Text("go to login"),
+    );
   }
 }
