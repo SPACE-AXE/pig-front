81e041e45cc9f842b99c2fc94ce62354eddee25c
iipmango <andykwon36@gmail.com>
2024-06-01 14:47:44 +0900

    이용기록 없을 때 예외처리 추가

--- lib/Screen/user/widget/slider_widget.dart
+++ lib/Screen/user/widget/slider_widget.dart
@@ -27,6 +27,7 @@ class _MyCarouselSliderState extends State<MyCarouselSlider> {
   @override
   void initState() {
     super.initState();
+    debugPrint("${widget.userData.accessToken}");
     getData();
   }
 
@@ -36,165 +37,174 @@ class _MyCarouselSliderState extends State<MyCarouselSlider> {
         ? const Center(child: CircularProgressIndicator())
         : SizedBox(
             height: MediaQuery.of(context).size.height * 0.3,
-            child: Stack(children: [
-              CarouselSlider(
-                carouselController: _controller,
-                items: data.map(
-                  (value) {
-                    final time = DateFormat('yyyy.MM.dd. HH:MM')
-                        .format(DateTime.parse(value['entryTime']));
-                    return Builder(
-                      builder: (context) {
-                        return Container(
-                          width: MediaQuery.of(context).size.width * 0.8,
-                          padding: const EdgeInsets.all(20),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: const Color(0xff39c5bb),
-                            ),
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Container(
+            child: isEmpty
+                ? const Text(
+                    "이용 기록이 없습니다.",
+                    style: TextStyle(
+                      fontSize: 25,
+                    ),
+                  )
+                : Stack(children: [
+                    CarouselSlider(
+                      carouselController: _controller,
+                      items: data.map(
+                        (value) {
+                          final time = DateFormat('yyyy.MM.dd. HH:MM')
+                              .format(DateTime.parse(value['entryTime']));
+                          return Builder(
+                            builder: (context) {
+                              return Container(
                                 width: MediaQuery.of(context).size.width * 0.8,
-                                decoration: const BoxDecoration(
-                                  border: Border(
-                                    bottom: BorderSide(
-                                      color: Color(0xff39c5bb),
-                                    ),
+                                padding: const EdgeInsets.all(20),
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: const Color(0xff39c5bb),
                                   ),
+                                  borderRadius: BorderRadius.circular(15),
                                 ),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
                                   children: [
-                                    const Text(
-                                      "이용기록",
-                                      style: TextStyle(fontSize: 20),
-                                    ),
-                                    IconButton(
-                                      icon: const Icon(Icons.add),
-                                      onPressed: () {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) =>
-                                                UsedScreen(), // UsedScreen으로 이동
+                                    Container(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.8,
+                                      decoration: const BoxDecoration(
+                                        border: Border(
+                                          bottom: BorderSide(
+                                            color: Color(0xff39c5bb),
                                           ),
-                                        );
-                                      },
+                                        ),
+                                      ),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          const Text(
+                                            "이용기록",
+                                            style: TextStyle(fontSize: 20),
+                                          ),
+                                          IconButton(
+                                            icon: const Icon(Icons.add),
+                                            onPressed: () {
+                                              Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      UsedScreen(), // UsedScreen으로 이동
+                                                ),
+                                              );
+                                            },
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        const Text(
+                                          "결제 시간",
+                                          style: TextStyle(fontSize: 20),
+                                        ),
+                                        Text(
+                                          DateFormat('yyyy.MM.dd. HH:MM')
+                                              .format(DateTime.parse(
+                                                  value['paymentTime'])),
+                                          style: TextStyle(
+                                            fontSize: 10,
+                                            color:
+                                                Colors.black.withOpacity(0.5),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        const Text(
+                                          "주차 비용",
+                                          style: TextStyle(fontSize: 20),
+                                        ),
+                                        Text(
+                                          "${value['parkingAmount']}",
+                                          style: const TextStyle(fontSize: 20),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        const Text(
+                                          "충전 비용",
+                                          style: TextStyle(fontSize: 20),
+                                        ),
+                                        Text(
+                                          "${value['chargeAmount'] ?? "0"}",
+                                          style: const TextStyle(fontSize: 20),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        const Text(
+                                          "총액",
+                                          style: TextStyle(fontSize: 20),
+                                        ),
+                                        Text(
+                                          "${value['totalAmount']}",
+                                          style: const TextStyle(fontSize: 20),
+                                        ),
+                                      ],
                                     ),
                                   ],
                                 ),
+                              );
+                            },
+                          );
+                        },
+                      ).toList(),
+                      options: CarouselOptions(
+                        autoPlay: true,
+                        viewportFraction: 1.0,
+                        onPageChanged: (index, reason) {
+                          setState(
+                            () {
+                              _current = index;
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: data.asMap().entries.map(
+                          (entry) {
+                            return GestureDetector(
+                              onTap: () => _controller.animateToPage(entry.key),
+                              child: Container(
+                                width: 10,
+                                height: 10,
+                                margin: const EdgeInsets.symmetric(
+                                    vertical: 8.0, horizontal: 4.0),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.black.withOpacity(
+                                      _current == entry.key ? 0.9 : 0.4),
+                                ),
                               ),
-                              const SizedBox(height: 10),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  const Text(
-                                    "결제 시간",
-                                    style: TextStyle(fontSize: 20),
-                                  ),
-                                  Text(
-                                    DateFormat('yyyy.MM.dd. HH:MM').format(
-                                        DateTime.parse(value['paymentTime'])),
-                                    style: TextStyle(
-                                      fontSize: 10,
-                                      color: Colors.black.withOpacity(0.5),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  const Text(
-                                    "주차 비용",
-                                    style: TextStyle(fontSize: 20),
-                                  ),
-                                  Text(
-                                    "${value['parkingAmount']}",
-                                    style: const TextStyle(fontSize: 20),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  const Text(
-                                    "충전 비용",
-                                    style: TextStyle(fontSize: 20),
-                                  ),
-                                  Text(
-                                    "${value['chargeAmount'] ?? "0"}",
-                                    style: const TextStyle(fontSize: 20),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  const Text(
-                                    "총액",
-                                    style: TextStyle(fontSize: 20),
-                                  ),
-                                  Text(
-                                    "${value['totalAmount']}",
-                                    style: const TextStyle(fontSize: 20),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ).toList(),
-                options: CarouselOptions(
-                  autoPlay: true,
-                  viewportFraction: 1.0,
-                  onPageChanged: (index, reason) {
-                    setState(
-                      () {
-                        _current = index;
-                      },
-                    );
-                  },
-                ),
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: data.asMap().entries.map(
-                    (entry) {
-                      print("$entry");
-                      return GestureDetector(
-                        onTap: () => _controller.animateToPage(entry.key),
-                        child: Container(
-                          width: 10,
-                          height: 10,
-                          margin: const EdgeInsets.symmetric(
-                              vertical: 8.0, horizontal: 4.0),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.black
-                                .withOpacity(_current == entry.key ? 0.9 : 0.4),
-                          ),
-                        ),
-                      );
-                    },
-                  ).toList(),
-                ),
-              )
-            ]),
+                            );
+                          },
+                        ).toList(),
+                      ),
+                    )
+                  ]),
           );
   }
 
@@ -212,7 +222,7 @@ class _MyCarouselSliderState extends State<MyCarouselSlider> {
             isLoading = false;
           });
           List<dynamic> tmp = response;
-          debugPrint("$tmp");
+          debugPrint("tmp:$response");
           if (tmp.isEmpty) {
             setState(() {
               isEmpty = true;
